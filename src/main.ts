19ce0c5a4788ab88b81ba6723d489dcaa51478f0
Otsute <rahatpracharktam@gmail.com>
2026-05-17 23:58:31 +1000

    I gave it my best shot

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,98 @@
-import { MarkdownPostProcessorContext, Plugin } from "obsidian";
+import { MarkdownView, Plugin } from "obsidian";
 
 export default class VariablesMD extends Plugin {
   async onload() {
-    this.registerMarkdownPostProcessor((element, _context) => {
-      // Visit every descendant nodes and modify any {{var}} to var.value
+    this.registerEvent(metadataCacheChangeEvent(this));
+    this.registerMarkdownCodeBlockProcessor("var", (source, el, _ctx) => {
       let file = this.app.workspace.getActiveFile();
-      file &&
+      if (file) {
         this.app.fileManager.processFrontMatter(file, (frontmatter) => {
-          this.replaceVariables(element, frontmatter);
+          let newSource = source.replaceAll(
+            /\{\{([^\}]*)\}\}/g,
+            (match, p1) => {
+              return p1 in frontmatter ? frontmatter[p1] : match;
+            },
+          );
+
+          // Construct a copy of the code block element with modified content
+          let div = el.createDiv();
+          let first = div.createDiv({
+            cls: [
+              "HyperMD-codeblock",
+              "HyperMD-codeblock-begin",
+              "HyperMD-codeblock-begin-bg",
+              "HyperMD-codeblock-bg",
+              "cm-line",
+            ],
+            attr: { dir: "ltr" },
+          });
+          first.createEl("img", {
+            cls: "cm-widgetBuffer",
+            attr: { "aria-hidden": "true" },
+          });
+          first.createSpan({ attr: { contenteditable: "false" } });
+          first.createEl("img", {
+            cls: "cm-widgetBuffer",
+            attr: { "aria-hidden": "true" },
+          });
+          first.createSpan({
+            cls: "code-block-flair",
+            attr: { "aria-label": "Copy", contenteditable: "false" },
+          });
+          first.createEl("img", {
+            cls: "cm-widgetBuffer",
+            attr: { "aria-hidden": "true" },
+          });
+
+          let second = div.createDiv({
+            cls: ["HyperMD-codeblock", "HyperMD-codeblock-bg", "cm-line"],
+          });
+          second.createSpan({
+            text: newSource,
+            cls: "cm-hmd-codeblock",
+            attr: { spellcheck: "false" },
+          });
+
+          let third = div.createDiv({
+            cls: [
+              "HyperMD-codeblock",
+              "HyperMD-codeblock-bg",
+              "HyperMD-codeblock-end",
+              "HyperMD-codeblock-end-bg",
+              "cm-line",
+            ],
+          });
+          third.setAttribute("dir", "ltr");
+          third.createEl("img", {
+            attr: { "aria-hidden": "true" },
+            cls: "cm-widgetBuffer",
+          });
+          third.createSpan({ attr: { contenteditable: "false" } });
+          third.createEl("img", {
+            attr: { "aria-hidden": "true" },
+            cls: "cm-widgetBuffer",
+          });
         });
+      }
     });
   }
 
-  async replaceVariables(node: Node, frontmatter: any) {
-    if (node.nodeType === 3 && node.nodeValue) {
-      let text = node.nodeValue;
-      let newText = text.replaceAll(
-        /\{\{([^\}]*)\}\}/g,
-        function replacer(match, p1) {
-          return p1 in frontmatter ? frontmatter[p1] : match;
-        },
-      );
-      node.nodeValue = newText;
-    } else {
-      node.childNodes.forEach((node) =>
-        this.replaceVariables(node, frontmatter),
-      );
-    }
-  }
-
-  async onunload() {}
+  onunload() {}
 }
+
+const metadataCacheChangeEvent = (plugin: VariablesMD) =>
+  plugin.app.metadataCache.on("changed", (path, _, _cache) => {
+    const activeFile = plugin.app.workspace.getActiveFile();
+    if (activeFile && activeFile === path) {
+      let view = plugin.app.workspace.getActiveViewOfType(MarkdownView);
+      if (view) {
+        let editor = view.editor;
+        let text = editor.getValue();
+        editor.replaceRange(
+          text,
+          editor.offsetToPos(0),
+          editor.offsetToPos(text.length),
+        );
+      }
+    }
+  });
